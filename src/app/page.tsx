6eeb557dedb2ef36a26f295bf3649a3f7af6b97f
Gina Vasiloff <ginavasiloff@gmail.com>
2024-08-08 12:28:46 -0600

    Add bio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,79 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import styles from './page.module.css'
+
+const skills = [
+  'Typescript',
+  'Docker',
+  'JavaScript',
+  'C#',
+  'Kubernetes',
+  'Unit Testing',
+  'Integration Testing',
+  'Pair Programming',
+  'Node',
+  'React',
+  'RESTful APIs',
+  'Debugging',
+  'Next.js',
+  'Data Visualizations',
+  'CSS',
+  'HTML',
+  'SQL',
+  'git',
+]
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+      <section>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/gina.jpg"
+          alt="Gina enjoying the sea spray at Milford Sound"
+          width={350}
+          height={554}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className={styles.bio}>
+          <div>
+            <h3>About Me</h3>
+            <p>
+              I love to build projects and try out new ideas. This has made
+              software engineering a rewarding career for me.
+            </p>
+            <p>
+              Web application development has been my focus the last several
+              years. I have been building analytics systems through the full
+              stack. I would love to talk to you in detail about my recent work
+              such as improving Postgres query performance, building a widget
+              dashboard, and architecting then implementing a cloud system for
+              automated analytics reports.
+            </p>
+            <p>
+              I became interested in software engineering by writing scripts for
+              a text based game. Then while studying computer science in
+              college, I got a job at a high energy astrophysics experiment at
+              the University of Utah. The experiment had a great need for a
+              floating software engineer; after building and deploying their
+              website, I went on to write data analysis scripts in Python,
+              perform laboratory data acquisition, and design and carry out
+              instrument calibration processes.
+            </p>
+            <p>
+              I have gone on to focus on web applicatons development and
+              architecture. My beginning in a scientific environment has led me
+              to focus on ensuring a shared understanding of product
+              requirements, accuracy, and reliability.
+            </p>
+          </div>
+          <div className={styles.skills}>
+            <h3>Skills</h3>
+            <ul>
+              {skills.map((s, i) => (
+                <li key={`skill-${i}`}>{s}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </section>
     </main>
-  );
+  )
 }
